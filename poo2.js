a4df6f60babe7d44e66de55f25b364daff7c90c9
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-08-15 08:59:53 -0300

    feat: Alterar objeto como se faz em uma POO

--- poo2.js
+++ poo2.js
@@ -1,7 +1,7 @@
 const pessoa = {
     primeiroNome: "Pablo",
     sobrenome: "Delgado",
-    idade: 25,
+    idade: 16,
     corOlho: "castanho escuro",
     altura: 185,
     dataNascimento: "26/5/2008",
@@ -11,4 +11,11 @@ const pessoa = {
     sexo: "Masculino"
 };
 
-console.log("O " + pessoa.primeiroNome + " " + pessoa.sobrenome + " tem " + pessoa.idade + " anos.");
+// Primeira frase ^
+console.log("1 - O " + pessoa.primeiroNome + " " + pessoa.sobrenome + " tem " + pessoa.idade + " anos.");
+
+// Alterar objeto
+pessoa.sobrenome = "Junior"
+
+// Segunda frase
+console.log("2 - O " + pessoa.primeiroNome + " " + pessoa.sobrenome + " tem " + pessoa.idade + " anos.");
