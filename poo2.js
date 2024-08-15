9b02887aadf15eda2160f8c0527eeb2e738caf87
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-08-15 08:51:36 -0300

    feat: Criando uma POO de caracteristica de pessoa

---
+++ poo2.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    primeiroNome: "Pablo",
+    sobrenome: "Delgado",
+    idade: 25,
+    corOlho: "castanho escuro",
+    altura: 185,
+    dataNascimento: "26/5/2008",
+    usaÓculos: false,
+    corPele:"CaféComLeite",
+    corFavorita: "Vermelho",
+    sexo: "Masculino"
+};
+
+console.log("O " + pessoa.primeiroNome + " " + pessoa.sobrenome + " tem " + pessoa.idade + " anos.");
