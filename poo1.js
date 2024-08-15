414853ca95a30bd0d35ac30390f4072d34a5cd7c
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-08-15 08:41:35 -0300

    feat: aula POO

---
+++ poo1.js
@@ -0,0 +1,16 @@
+const animal = {
+    nome: "Pantera cor de rosa",
+    raca: "vira lata",
+    porte: "médio",
+    cor: "rosa",
+    sexo: "macho",
+    agressivo: true
+};
+console.log(animal.nome);
+
+if (animal.agressivo == true) {
+    console.log("cachorro louco");
+}else{
+    console.log("miau");
+    
+}
